c3e8aa64bf722cfe482b3bcfb661d1103491af33
Yaroslav Kravchenko <yaroslav.kravchenko@hotmail.com>
2014-03-24 15:02:40 +0400

    New structure

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ var Tags = require('./tags');
 var create = function () {
 	var units = new UnitSet();
 
-	units.addSet("post", postUnits.create());
-	units.addSet('node', nodeUnits.create());
-	units.add('tag', new Tags());
+	units.addSet('post', postUnits.create());
+	units.addSet('post.node', nodeUnits.create());
+	units.add('post.tags', new Tags());
 
 	return units;
 };
