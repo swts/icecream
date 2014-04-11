268fb73a48564855ac973097f65ae807a9f4fc69
Alexey Novikov <velocityzen@gmail.com>
2014-04-11 21:32:54 +0400

    Fixed bug with category filter

--- resources/post/controller.js
+++ resources/post/controller.js
@@ -101,7 +101,7 @@ Post.prototype.getByCategory = function(category, options, cb) {
 	if (category !== "all" && category !== undefined) {
 		ql.unshift({
 			filter: function(row) {
-				return row('categories').hasFields(category);
+				return row('categories').contains(category);
 			}
 		});
 	}
